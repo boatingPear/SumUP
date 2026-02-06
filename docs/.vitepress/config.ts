f93327230f5dc139407d173e505e93df45c865a9
一只划船的猪 <8948212+dabai-pear@user.noreply.gitee.com>
2026-02-06 15:52:11 +0800

    1

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vitepress'
+import nav from "./nav"
+import sidebar from "./sidebar"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "SumUP",
+  description: "个人总结",
+  head: [],
+  lang: 'zh-CN',
+  // 所有的文档都放在src目录下
+  srcDir: './src',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // 顶部导航菜单
+    nav,
+    // 侧边栏菜单
+    sidebar,
+    // 文档页脚文本
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
+    // 文档搜索
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: "搜索文档",
+                buttonAriaLabel: "搜索文档",
+              },
+              modal: {
+                noResultsText: "无法找到相关结果",
+                resetButtonTitle: "清除查询条件",
+                footer: {
+                  selectText: "选择",
+                  navigateText: "切换",
+                  closeText: "关闭",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    // 社交链接
+    socialLinks: [
+      { icon: 'gitee', link: 'https://gitee.com/dabai-pear' },
+      { icon: 'github', link: 'https://github.com/boatingPear' }
+    ]
+  }
+})
