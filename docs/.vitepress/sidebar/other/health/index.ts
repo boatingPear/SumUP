75964fb521e830119c99176ec9f2efb6b5b5c52d
wzq <1324573144@qq.com>
2026-04-28 22:59:32 +0800

    1

--- docs/.vitepress/sidebar/other/health/index.ts
+++ docs/.vitepress/sidebar/other/health/index.ts
@@ -5,4 +5,8 @@ export default <DefaultTheme.SidebarItem>[
         text: "健身训练动作及计划",
         link: "/other/health/exercise.md",
     },
+    {
+        text: "饮食计划",
+        link: "/other/health/diet.md",
+    }
 ]
