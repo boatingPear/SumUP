3999d1ec82961f9d15cb60f20ab74706a962f301
一只划船的猪 <1324573144@qq.com>
2026-07-09 23:36:52 +0800

    2

--- docs/.vitepress/sidebar/other/health/index.ts
+++ docs/.vitepress/sidebar/other/health/index.ts
@@ -2,8 +2,20 @@ import type { DefaultTheme } from "vitepress"
 
 export default <DefaultTheme.SidebarItem>[
     {
-        text: "训练计划",
+        text: "训练计划（总览）",
         link: "/other/health/training-plan.md"
+    },
+    {
+        text: "第一天：背部 + 后束 + 二头（拉）",
+        link: "/other/health/training-plan/day1-push.md"
+    },
+    {
+        text: "第二天：胸部 + 肩部 + 三头（推）",
+        link: "/other/health/training-plan/day2-pull.md"
+    },
+    {
+        text: "第三天：下肢 + 核心",
+        link: "/other/health/training-plan/day3-legs.md"
     },
     {
         text: "健身训练动作及计划",
